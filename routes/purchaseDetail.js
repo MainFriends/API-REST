aeb60bf0638354828f6dcada6b296fda25637b4f
Hackermaster <jgmedina221@gmail.com>
2022-03-13 21:55:20 -0600

    todavia no funcionaban las rutas

--- routes/purchaseDetail.js
+++ routes/purchaseDetail.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const purchaseDetailController = require('../controllers/purchaseDetailController');
 
-/*router.get('/purchases-detail', purchaseDetailController.getDetail);
-/*router.get('/purchase-detail/:codDetail', purchaseDetailController.getDetail);
-/*router.post('/purchase-detail', purchaseDetailController.addDetail);
-/*router.put('/purchase-detail/:codDetail', purchaseDetailController.updateDetail);
-router.delete('/purchase-detail/:codDetail', purchaseDetailController.deleteDetail);*/
+
+router.get('/purchase-detail/:codDetail', purchaseDetailController.getDetail);
+router.post('/purchase-detail', purchaseDetailController.addDetail);
+router.put('/purchase-detail/:codDetail', purchaseDetailController.updateDetail);
+router.delete('/purchase-detail/:codDetail', purchaseDetailController.deleteDetail);
 
 module.exports = router;
