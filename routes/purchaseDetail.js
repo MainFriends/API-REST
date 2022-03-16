362d94fc241b5431f1bee71175e1e0383821643c
Hackermaster <jgmedina221@gmail.com>
2022-03-15 20:47:49 -0600

    se agregor user extractor a purchase  detail e invoice

--- routes/purchaseDetail.js
+++ routes/purchaseDetail.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const purchaseDetailController = require('../controllers/purchaseDetailController');
+const userExtractor = require('../middlewares/userExtractor');
 
-router.get('/purchase-detail/:codDetail', purchaseDetailController.getDetail);
-router.post('/purchase-detail', purchaseDetailController.addDetail);
-router.put('/purchase-detail/:codDetail', purchaseDetailController.updateDetail);
-router.delete('/purchase-detail/:codDetail', purchaseDetailController.deleteDetail);
+router.get('/purchase-detail/:codDetail',userExtractor, purchaseDetailController.getDetail);
+router.post('/purchase-detail',userExtractor, purchaseDetailController.addDetail);
+router.put('/purchase-detail/:codDetail',userExtractor, purchaseDetailController.updateDetail);
+router.delete('/purchase-detail/:codDetail',userExtractor, purchaseDetailController.deleteDetail);
 
 module.exports = router;
